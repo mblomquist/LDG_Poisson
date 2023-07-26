eff5d9d04ae1a2392a52efef499a754142ce5913
mblomquist <mblomquist@ucmerced.edu>
2023-07-26 15:33:20 -0700

    Add private variables for tree class.

--- src/treegrid.hpp
+++ src/treegrid.hpp
@@ -15,6 +15,12 @@ class treeGrid
         int type = 0;
         algoim::uvector<double, N> xmin, xmax;
     };
+
+
+    int min_level = 0;
+    int max_level = 15;
+
+    algoim::uvector<double, N> domain_min, domain_max;
 };
 
 #endif //LDG_POISSON_TREEGRID_HPP
